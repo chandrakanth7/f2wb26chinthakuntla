bcf0eec845b9205231fccda588c39101f97c4d6c
chandrakanth7 <42292161+chandrakanth7@users.noreply.github.com>
2022-10-31 16:34:57 -0500

    worked on assignment 10

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chickenDishRouter = require('./routes/chickenDish');
+var gridBuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chickenDish', chickenDishRouter);
+app.use('/gridbuild', gridBuildRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
